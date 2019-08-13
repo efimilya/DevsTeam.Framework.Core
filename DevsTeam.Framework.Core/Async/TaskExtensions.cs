802993528ff9bc596a23d8b6569cd6a9551e8572
Ilya Efimov <efimilya@gmail.com>
2019-08-13 20:48:08 +0300

    DoNotWait task extension method added

---
+++ DevsTeam.Framework.Core/Async/TaskExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace DevsTeam.Framework.Core.Async
+{
+    public static class TaskExtensions
+    {
+        public static async void DoNotWait(this Task task) => await task;
+    }
+}
