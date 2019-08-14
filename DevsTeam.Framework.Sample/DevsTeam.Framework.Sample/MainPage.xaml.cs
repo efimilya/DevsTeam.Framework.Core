f0d3ea63633344a8847b6cc4bcd26551d72e773a
Ilya Efimov <efimilya@gmail.com>
2019-08-14 10:35:22 +0300

    Added Sample project

---
+++ DevsTeam.Framework.Sample/DevsTeam.Framework.Sample/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevsTeam.Framework.Core.Async;
+using Xamarin.Forms;
+
+namespace DevsTeam.Framework.Sample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            T().DoNotWait();
+        }
+
+        private async Task T()
+        {
+            throw new NotSupportedException("s;kla;sldkfj");
+        }
+    }
+}
