6711447b06ec7bac88594924bfef62a6e2898300
Ilya Efimov <efimilya@gmail.com>
2019-08-14 10:34:32 +0300

    Make PeriodicOperation nested

--- DevsTeam.Framework.Core/Periodic/PeriodicContainer.cs
+++ DevsTeam.Framework.Core/Periodic/PeriodicContainer.cs
@@ -1,27 +1,26 @@
 ﻿using System;
 using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using DevsTeam.Framework.Core.Async;
+using ReactiveUI;
 
 namespace DevsTeam.Framework.Core.Periodic
 {
     // ReSharper disable once ClassNeverInstantiated.Global
-    public class PeriodicContainer : IDisposable
+    public sealed class PeriodicContainer : IDisposable
     {
         private readonly CompositeDisposable _lifetime;
-        private readonly Func<PeriodicOperation> _periodicOperationFactory;
         private volatile bool _isSuspended;
 
-        public PeriodicContainer(CompositeDisposable lifetime, Func<PeriodicOperation> periodicOperationFactory)
-        {
-            _lifetime = lifetime;
-            _periodicOperationFactory = periodicOperationFactory;
-        }
+        public PeriodicContainer() => _lifetime = new CompositeDisposable();
 
         public void Dispose() => _lifetime.Dispose();
 
         public IDisposable Register(TimeSpan interval, Func<Task> action, bool performOnMainThread)
         {
-            var subscription = _periodicOperationFactory().Start(interval, action, performOnMainThread, () => _isSuspended);
+            var subscription = new PeriodicOperation().Start(interval, action, performOnMainThread, () => _isSuspended);
             _lifetime.Add(subscription);
             return subscription;
         }
@@ -29,5 +28,31 @@ namespace DevsTeam.Framework.Core.Periodic
         public void Resume() => _isSuspended = false;
 
         public void Suspend() => _isSuspended = true;
+
+        private class PeriodicOperation
+        {
+            private int _isPerforming;
+
+            public IDisposable Start(TimeSpan interval, Func<Task> action, bool performOnMainThread, Func<bool> isSuspended)
+            {
+                var observable = Observable.Interval(interval).Where(_ => !isSuspended());
+                if (performOnMainThread) observable = observable.ObserveOn(RxApp.MainThreadScheduler);
+                return observable.Subscribe(_ => Perform(action));
+            }
+
+            private async Task Perform(Func<Task> action)
+            {
+                if (Interlocked.CompareExchange(ref _isPerforming, 1, 0) == 1) return;
+
+                try
+                {
+                    await action();
+                }
+                finally
+                {
+                    Interlocked.Exchange(ref _isPerforming, 0);
+                }
+            }
+        }
     }
 }
